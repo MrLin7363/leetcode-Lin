53b9eabc299d1fbdb679ccd98efe3225f8f4a694
JunLin <m13877977363@163.com>
2020-12-14 21:31:21 +0800

    Junlin  2020/12/14

--- src/Linked_List/medium/P92_Reverse_Linked_List_II.java
+++ src/Linked_List/medium/P92_Reverse_Linked_List_II.java
@@ -10,25 +10,21 @@ import Construct.ListNode;
 
 public class P92_Reverse_Linked_List_II {
     /*
-
+    迭代反转，先找到前驱结点，在反转剩余的节点数
      */
     public ListNode reverseBetween(ListNode head, int m, int n) {
         if (head == null) {
             return head;
         }
-        ListNode next = null;
-        int begin = 0, end = 0;
-        while (head != null ) {
-            head=head.next;
-            begin++;
-            end++;
-            if (begin==m) {
-                ListNode pre = head;
-            }
-            if (end==n+1){
-                break;
-            }
+        ListNode cur = head, prev = null;
+        while (m>0){
+            prev=cur;
+            cur=cur.next;
+            m--;
+            n--;
         }
+        ListNode con = prev, tail = cur;
+
         return null;
     }
 }
