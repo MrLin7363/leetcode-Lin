564579c1d06a9b6d61e8d70ea6995ecd78e4812a
JunLin <m13877977363@163.com>
2021-01-07 11:22:56 +0800

    Junlin thinkpad 2021/01/07

--- src/Bit_Manipulation/easy/P169_Majority_Element.java
+++ src/Bit_Manipulation/easy/P169_Majority_Element.java
@@ -25,10 +25,40 @@ public class P169_Majority_Element {
         }
         return candidate;
     }
+
+    /*
+    分治算法  99 + 78
+     */
+    public int majorityElement(int[] nums) {
+        return majorityElementRec(nums,0,nums.length-1);
+    }
+    public int majorityElementRec(int[] nums,int low,int high){
+        if (low==high){
+            return nums[low];
+        }
+        int mid=(high-low)/2+low;
+        int left = majorityElementRec(nums,low,mid);
+        int right = majorityElementRec(nums,mid+1,high);
+        if (left==right){
+            return left;
+        }
+        int leftCount=countInRange(nums,left,low,high);
+        int rightCount = countInRange(nums, right, low, high);
+        return leftCount>rightCount ? left:right;
+    }
+    public int countInRange(int[] nums,int num,int low,int high){
+        int count=0;
+        for (int i = low; i <= high; i++) {
+            if (nums[i]==num){
+                count++;
+            }
+        }
+        return count;
+    }
     /*
     hashmap 47 + 17
      */
-    public int majorityElement(int[] nums) {
+    public int majorityElement3(int[] nums) {
         Map<Integer,Integer> counts=countNums(nums);
         Map.Entry<Integer, Integer> majorityEntry = null;
         for (Map.Entry<Integer,Integer> entry:counts.entrySet()){
