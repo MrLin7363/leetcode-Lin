f4cd2e02ddf568f6d8da1d8728b5e6f4cb9daf3f
chen junlin <chenjunlin@yulong.com>
2021-08-09 16:37:49 +0800

    2021-8-9 house robber

---
+++ src/DynamicProgramming/medium/P198_House_Robber.java
@@ -0,0 +1,26 @@
+package DynamicProgramming.medium;
+
+/**
+ * @author: chenjunlin
+ * @since: 2021/08/09
+ * @descripe: 相邻不能偷
+ */
+public class P198_House_Robber {
+
+    public static int rob(int[] nums) {
+        int[] dp = new int[nums.length];
+        if (nums.length==1){
+            return nums[0];
+        }
+        if (nums.length==2){
+            return Math.max(nums[0],nums[1]);
+        }
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0],nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i-1],dp[i-2]+nums[i]);
+        }
+        return dp[nums.length-1];
+    }
+
+}
---
+++ src/DynamicProgramming/medium/P213_House_Robber_II.java
@@ -0,0 +1,14 @@
+package DynamicProgramming.medium;
+
+/**
+ * @author: chenjunlin
+ * @since: 2021/08/09
+ * @descripe: 相邻不能偷，数组是个环，最后一个和第一个是接着的
+ */
+public class P213_House_Robber_II {
+
+    public int rob(int[] nums) {
+        return 0;
+    }
+
+}
