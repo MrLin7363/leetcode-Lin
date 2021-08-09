61a350ef4c4ca1ef0bd7861cf63cc16bf7e79fc4
chen junlin <chenjunlin@yulong.com>
2021-08-09 19:00:09 +0800

    2021-8-9

--- src/DynamicProgramming/medium/P213_House_Robber_II.java
+++ src/DynamicProgramming/medium/P213_House_Robber_II.java
@@ -8,7 +8,20 @@ package DynamicProgramming.medium;
 public class P213_House_Robber_II {
 
     public int rob(int[] nums) {
-        return 0;
+        int n = nums.length;
+        int[] dp = new int[n];
+        if (n==1){
+            return nums[0];
+        }
+        if (n==2){
+            return 0;
+        }
+        dp[0] = nums[0];
+        dp[1] = nums[1];
+        for (int i = 2; i < n-1; i++) {
+            dp[i] = Math.max(dp[i-1],dp[i-2]+nums[i]);
+        }
+        return dp[n-1];
     }
 
 }
