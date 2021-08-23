d0b81f77129e20757dfa7f4df1942d15e86d1087
junlin_chen <m13877977363@163.com>
2021-08-23 22:46:32 +0800

    2021-8-23

--- src/DynamicProgramming/medium/P213_House_Robber_II.java
+++ src/DynamicProgramming/medium/P213_House_Robber_II.java
@@ -7,8 +7,30 @@ package DynamicProgramming.medium;
  */
 public class P213_House_Robber_II {
 
-    public int rob(int[] nums) {
-        return 0;
+    public static int rob(int[] nums) {
+        int[] dp = new int[nums.length];
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        if (nums.length == 2) {
+            return 0;
+        }
+        int n = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            if (n-2-1==i){
+                dp[i] = nums[i];
+                continue;
+            }
+            dp[i] = Math.max(dp[i - 1 < 0 ? n - 1 : i - 1], dp[i - 2 < 0 ? n - i-2 : i - 1] + nums[i]);
+        }
+        int res=0;
+        for (int i = 0; i < dp.length; i++) {
+            res=Math.max(dp[i],res);
+        }
+        return res;
     }
 
+    public static void main(String[] args) {
+        rob(new int[]{2,3,2});
+    }
 }
