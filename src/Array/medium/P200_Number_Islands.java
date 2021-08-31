edf61b398c3ef28f04bd74afaf5288aa483485ab
chen junlin <chenjunlin@yulong.com>
2021-08-31 16:39:25 +0800

    2021-8-31

--- src/Array/medium/P200_Number_Islands.java
+++ src/Array/medium/P200_Number_Islands.java
@@ -8,10 +8,32 @@ package Array.medium;
 public class P200_Number_Islands {
 
     /*
+    DFS
     探测到一个岛，向四周扩散，将岛屿的 1 > 0 被水填满
+    99 + 94
      */
+    int nums=0;
     public int numIslands(char[][] grid) {
-        return 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j]=='1') {
+                    nums++;
+                    dfsExpandIslands(grid, i, j);
+                }
+            }
+        }
+        return nums;
+    }
+
+    private void dfsExpandIslands(char[][] grid,int i,int j){
+        if (i<0||j<0||i>=grid.length||j>=grid[0].length||grid[i][j]=='0'){
+            return;
+        }
+        grid[i][j]='0';
+        dfsExpandIslands(grid,i+1,j);
+        dfsExpandIslands(grid,i-1,j);
+        dfsExpandIslands(grid,i,j+1);
+        dfsExpandIslands(grid,i,j-1);
     }
 
 }
