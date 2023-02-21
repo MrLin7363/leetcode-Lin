83a867c2d340eea635569c18bd453c8bccbf8c44
Junlin <49462524+13877977363@users.noreply.github.com>
2023-02-21 11:10:33 +0800

    Add files via upload

---
+++ src/A2023/路径算法/P743网络延迟时间DijkstraHeap.java
@@ -0,0 +1,23 @@
+package A2023.路径算法;
+
+import TenSortAlgorithm.path.DijistraHeapMy;
+
+/**
+ * Dijkstra - 堆优化/邻接表
+ */
+public class P743网络延迟时间DijkstraHeap {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        int INF = Integer.MAX_VALUE;
+        // 从0下标开始
+        int[] dist = new DijistraHeapMy().dijkstra(times, n, k - 1);
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < dist.length; i++) {
+            max = Math.max(max, dist[i]);
+        }
+        return max == INF ? -1 : max;
+    }
+
+    public static void main(String[] args) {
+        new P743网络延迟时间DijkstraHeap().networkDelayTime(new int[][]{{2, 1, 1}, {2, 3, 2}, {3, 4, 1}}, 4, 2);
+    }
+}
