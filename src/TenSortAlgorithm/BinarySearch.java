a983cbe628529ef12574393d0d87e23553f9cab4
Lin <49462524+MrLin7363@users.noreply.github.com>
2023-11-27 14:40:09 +0800

    Update BinarySearch.java

--- src/TenSortAlgorithm/BinarySearch.java
+++ src/TenSortAlgorithm/BinarySearch.java
@@ -15,8 +15,8 @@ public class BinarySearch {
         int left = 0;
         int right = nums.length - 1;
         while (left <= right) {
-            //            int mid=(left+right)/2;
-            int mid = left + (right - left) / 2;
+            //            int mid=(left+right)/2; 偶数偏左    (left+right+1)/2; //偶数偏右
+            int mid = left + (right - left) / 2;  // 偶数偏左
             if (target == nums[mid])
                 return mid;
             else if (nums[mid] < target)
@@ -27,24 +27,8 @@ public class BinarySearch {
         return -1;
     }
 
-    // 左闭右开：一般用于找边界问题，也可以二分搜索一个数如下：不推荐
-    //  右开说明右边是 )  right=mid-1  会错，这样 [left,mid-1)  mid-1就扫描不到了 ，所以right=mid
-    // int binarySearch2(int[] nums, int target) {
-    //     int left = 0;
-    //     int right = nums.length;
-    //     while (left < right) {
-    //         int mid = left + (right - left) / 2;
-    //         if (target == nums[mid])
-    //             return mid;
-    //         else if (nums[mid] < target)
-    //             left = mid + 1;
-    //         else if (nums[mid] > target)
-    //             right = mid;
-    //     }
-    //     return -1;
-    // }
 
-    // 左闭右开：搜索最左边界，返回下标  右开是因为right=length
+    // 左闭右开：搜索某个数的最左边界，返回下标  右开是因为right=length
     static int binarySearchByLeft(int[] nums, int target) {
         int left = 0;
         int right = nums.length;
@@ -58,13 +42,15 @@ public class BinarySearch {
                 right = mid;
         }
         return left; // 因为结束条件是 left==right 所以返回哪个都一样
-        // target 比所有数都大
-        //        if (left == nums.length) return -1;
-        // 类似之前算法的处理方式
-        //        return nums[left] == target ? left : -1;
+        /* 如果找不到需要返回-1的情况
+        if (left != nums.length && nums[left] == target) {
+            return left;
+        }
+        return -1;
+         */
     }
 
-    // 左闭右开：搜索最右边界，返回下标    右开是因为right=length
+    // 左闭右开：搜索某个数的最右边界，返回下标    右开是因为right=length
     static int binarySearchByRight(int[] nums, int target) {
         int left = 0;
         int right = nums.length;
@@ -80,12 +66,17 @@ public class BinarySearch {
         //又因为收紧左侧边界时必须 left = mid + 1
         //所以最后无论返回 left 还是 right，必须减一
         return right - 1;
+        /*
+        if (right != -1 && nums[right] == target) {
+            return right;
+        }
+         */
     }
 
     // 测试方法
     public static int testLeft() {
         int[] nums = new int[]{1, 1, 2};
-        int target = 3;
+        int target = 1;
         int leftResult = binarySearchByLeft(nums, target);
         // target 比所有数都大
         if (leftResult == nums.length)
